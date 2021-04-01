012857f532bdabce9339440053c3129200220c36
Andriy <andriy.andriyan1@gmail.com>
2021-04-02 00:12:40 +0700

    Add tawk to key

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
-  var tawkId = pluginOptions.tawkId
-  var source = "https://embed.tawk.to/" + tawkId + "/default"
-  
+exports.onRenderBody = ({ setPostBodyComponents }, { tawkId, tawkKey }) => {
+  const source = `https://embed.tawk.to/${tawkId}/${tawkKey}`;
+
   return setPostBodyComponents([
-    <script
-      key="gatsby-plugin-tawk.to"
-      src={source}
-    />
+    <script key="gatsby-plugin-tawk.to" src={source} />
   ]);
 };
